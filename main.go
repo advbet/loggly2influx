1569fa03c6569926aa248dabf143b415060027d2
Dovydas <SE.David@protonmail.com>
2022-03-28 16:39:04 +0300

    Update package to influx v2 version (#1)

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"flag"
@@ -8,80 +9,62 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 
-	"github.com/influxdata/influxdb/client/v2"
+	"github.com/coreos/go-systemd/daemon"
+	influxdb "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api"
+	"github.com/influxdata/influxdb-client-go/v2/api/write"
 	"github.com/sirupsen/logrus"
 )
+
+var conf struct {
+	Interval time.Duration
+	Loggly   struct {
+		Query   string
+		User    string
+		Pass    string
+		Token   string
+		Account string
+	}
+	Influx struct {
+		Prefix string
+		URL    string
+		Token  string
+		Org    string
+		Bucket string
+	}
+}
 
 type logglyClient struct {
 	baseURL string
 	client  *http.Client
 }
 
-var conf struct {
-	interval time.Duration
-	loggly   struct {
-		account string
-		user    string
-		pass    string
-		token   string
-		query   string
-	}
-	influx struct {
-		url      string
-		database string
-		prefix   string
-	}
-}
+func (lc *logglyClient) TagsCount(
+	ctx context.Context,
+	from, to time.Time,
+) (map[string]int, error) {
 
-//func (lc *logglyClient) MessageCount(from, to time.Time, tag string) (map[string]int, int, error) {
-//}
-
-func parseLogglyFieldCount(r io.Reader, field string) (map[string]int, error) {
-	var data map[string]json.RawMessage
-
-	if err := json.NewDecoder(r).Decode(&data); err != nil {
-		return nil, err
-	}
-
-	rawJSON, ok := data[field]
-	if !ok {
-		return nil, fmt.Errorf("missing field \"%s\" in response", field)
-	}
-
-	tagsJSON := []struct {
-		Count int    `json:"count"`
-		Term  string `json:"term"`
-	}{}
-
-	if err := json.Unmarshal(rawJSON, &tagsJSON); err != nil {
-		return nil, err
-	}
-
-	tags := make(map[string]int)
-	for _, tag := range tagsJSON {
-		tags[tag.Term] += tag.Count
-	}
-	return tags, nil
-}
-
-func (lc *logglyClient) TagsCount(field string, query string, from, to time.Time) (map[string]int, error) {
-	url := fmt.Sprintf("%s/fields/%s?q=%s&facet_size=20&from=%s&to=%s",
+	url := fmt.Sprintf("%s/fields/tag?q=%s&facet_size=20&from=%s&to=%s",
 		lc.baseURL,
-		field,
-		url.QueryEscape(query),
-		from.UTC().Format(time.RFC3339),
-		to.UTC().Format(time.RFC3339),
+		url.QueryEscape(conf.Loggly.Query),
+		from.UTC().Format(time.RFC3339), to.UTC().Format(time.RFC3339),
 	)
 
-	req, err := http.NewRequest("GET", url, nil)
-	if conf.loggly.token != "" {
-		req.Header.Add("Authorization", fmt.Sprintf("bearer %s", conf.loggly.token))
-	}
+	req, err := http.NewRequestWithContext(ctx, "GET", url, http.NoBody)
 	if err != nil {
 		return nil, err
 	}
+
+	if conf.Loggly.Token != "" {
+		req.Header.Add("Authorization", fmt.Sprintf("bearer %s", conf.Loggly.Token))
+	}
+
 	resp, err := lc.client.Do(req)
 	if err != nil {
 		return nil, err
@@ -89,131 +72,120 @@ func (lc *logglyClient) TagsCount(field string, query string, from, to time.Time
 	defer resp.Body.Close()
 
 	if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		return nil, errors.New(resp.Status)
+		return nil, fmt.Errorf("invalid response status: %s", resp.Status)
 	}
-	return parseLogglyFieldCount(resp.Body, field)
+
+	return parseLogglyFieldCount(resp.Body)
 }
 
-func writeTags(influx client.Client, tags map[string]int, at time.Time) error {
-	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
-		Database:  conf.influx.database,
-		Precision: "s",
-	})
-	if err != nil {
-		return err
+func parseLogglyFieldCount(r io.Reader) (map[string]int, error) {
+	var data struct {
+		Tag []struct {
+			Count int    `json:"count"`
+			Term  string `json:"term"`
+		} `json:"tag"`
 	}
 
-	for tag, count := range tags {
-		pt, err := client.NewPoint(
-			fmt.Sprintf("%stag_entries", conf.influx.prefix),
-			map[string]string{"tag": tag},
-			map[string]interface{}{"count": count},
-			at,
-		)
-		if err != nil {
-			return err
+	if err := json.NewDecoder(r).Decode(&data); err != nil {
+		return nil, err
+	}
+
+	if len(data.Tag) == 0 {
+		return nil, errors.New("missing tags in loggly response")
+	}
+
+	tags := make(map[string]int, len(data.Tag))
+	for _, tag := range data.Tag {
+		tags[tag.Term] += tag.Count
+	}
+
+	return tags, nil
+}
+
+func pollTags(
+	ctx context.Context,
+	rapi api.WriteAPI,
+	lc *logglyClient,
+	interval time.Duration,
+) {
+
+	var (
+		lastTstamp time.Time = time.Now()
+		currTstamp time.Time
+	)
+
+	tm := time.NewTimer(interval)
+	defer func() {
+		tm.Stop()
+		select {
+		case <-tm.C:
+		default:
 		}
-		bp.AddPoint(pt)
-	}
-	return influx.Write(bp)
-}
+	}()
 
-func writeMessages(influx client.Client, tag string, msgs map[string]int, at time.Time) error {
-	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
-		Database:  conf.influx.database,
-		Precision: "s",
-	})
-	if err != nil {
-		return err
-	}
-
-	for msg, count := range msgs {
-		pt, err := client.NewPoint(
-			fmt.Sprintf("%smessage_entries", conf.influx.prefix),
-			map[string]string{"tag": tag, "message": msg},
-			map[string]interface{}{"count": count},
-			at,
-		)
-		if err != nil {
-			return err
+	for ctx.Err() == nil {
+		select {
+		case currTstamp = <-tm.C:
+		case <-ctx.Done():
+			return
 		}
-		bp.AddPoint(pt)
-	}
-	return influx.Write(bp)
-}
 
-func loop(influx client.Client, lc *logglyClient, interval time.Duration) {
-	nextCheck := time.Now().Add(-1 * interval)
-
-	for {
-		logrus.WithField("duration", -1*time.Since(nextCheck)).Info("sleep")
-		time.Sleep(-1 * time.Since(nextCheck))
-		var tags map[string]int
-		for {
-			var err error
-			tags, err = lc.TagsCount("tag", conf.loggly.query, nextCheck.Add(-1*interval), nextCheck)
-			if err == nil {
-				break
+		tags, err := lc.TagsCount(ctx, lastTstamp, currTstamp)
+		if err != nil {
+			if errors.Is(err, ctx.Err()) {
+				return
 			}
-			logrus.WithError(err).Error("getting field stats from loggly")
-			time.Sleep(time.Second)
+
+			logrus.WithError(err).Error("getting tag stats from loggly")
+			tm.Reset(time.Second)
+			continue
 		}
+
 		logrus.WithFields(logrus.Fields{
 			"tags": tags,
-			"from": nextCheck.Add(-1 * interval),
-			"to":   nextCheck,
+			"from": lastTstamp,
+			"to":   currTstamp,
 		}).Info("received data from loggly")
-		for {
-			if err := writeTags(influx, tags, nextCheck); err != nil {
-				logrus.WithError(err).Error("writing data to influx")
-				time.Sleep(time.Second)
-				continue
-			}
-			break
-		}
-		/*
-			for tag := range tags {
-				var msgs map[string]int
-				for {
-					msgs, err = lc.TagsCount("json.message", "tag:"+tag, nextCheck.Add(-1*interval), nextCheck)
-					if err == nil {
-						break
-					}
-				}
-				logrus.WithField("messages", msgs).Info("data from loggly")
-				for {
-					if err := writeMessages(influx, tag, msgs, nextCheck); err == nil {
-						break
-					}
-					logrus.WithError(err).Error("writing data to influx")
-					time.Sleep(time.Second)
-				}
 
-			}
-		*/
-		nextCheck = nextCheck.Add(interval)
+		for tag, count := range tags {
+			point := write.NewPoint(
+				fmt.Sprintf("%stag_entries", conf.Influx.Prefix),
+				map[string]string{"tag": tag},
+				map[string]interface{}{"count": count},
+				currTstamp,
+			)
+
+			rapi.WritePoint(point)
+		}
+
+		rapi.Flush()
+		lastTstamp = currTstamp
+		tm.Reset(interval)
 	}
 }
 
 func main() {
-	flag.StringVar(&conf.loggly.account, "loggly-account", "", "Loggly account")
-	flag.StringVar(&conf.loggly.user, "loggly-user", "", "Loggly username")
-	flag.StringVar(&conf.loggly.pass, "loggly-pass", "", "Loggly password")
-	flag.StringVar(&conf.loggly.token, "loggly-token", "", "Loggly access token")
-	flag.StringVar(&conf.loggly.query, "loggly-query", "", "Query for data lookup, can be empty")
-	flag.StringVar(&conf.influx.url, "influx-url", "", "Influx DB URL")
-	flag.StringVar(&conf.influx.database, "influx-database", "", "Influx database name")
-	flag.StringVar(&conf.influx.prefix, "influx-prefix", "", "Prefix for all influx metrics names")
-	flag.DurationVar(&conf.interval, "interval", time.Minute, "Data poll interval")
+	flag.StringVar(&conf.Loggly.Account, "loggly-account", "", "Loggly account")
+	flag.StringVar(&conf.Loggly.User, "loggly-user", "", "Loggly username")
+	flag.StringVar(&conf.Loggly.Pass, "loggly-pass", "", "Loggly password")
+	flag.StringVar(&conf.Loggly.Token, "loggly-token", "", "Loggly access token")
+	flag.StringVar(&conf.Loggly.Query, "loggly-query", "", "Query for data lookup, can be empty")
+	flag.StringVar(&conf.Influx.URL, "influx-url", "", "Influx URL")
+	flag.StringVar(&conf.Influx.Token, "influx-token", "", "Influx token")
+	flag.StringVar(&conf.Influx.Org, "influx-org", "", "Influx organization")
+	flag.StringVar(&conf.Influx.Bucket, "influx-bucket", "", "Influx organization bucket")
+	flag.StringVar(&conf.Influx.Prefix, "influx-prefix", "", "Prefix for all influx metrics names")
+	flag.DurationVar(&conf.Interval, "interval", time.Minute, "Data poll interval")
 	flag.Parse()
 
 	baseURL := url.URL{
 		Scheme: "https",
-		Host:   fmt.Sprintf("%s.loggly.com", conf.loggly.account),
+		Host:   fmt.Sprintf("%s.loggly.com", conf.Loggly.Account),
 		Path:   "/apiv2",
 	}
-	if conf.loggly.pass != "" {
-		baseURL.User = url.UserPassword(conf.loggly.user, conf.loggly.pass)
+
+	if conf.Loggly.Pass != "" {
+		baseURL.User = url.UserPassword(conf.Loggly.User, conf.Loggly.Pass)
 	}
 
 	lc := &logglyClient{
@@ -221,14 +193,48 @@ func main() {
 		client:  http.DefaultClient,
 	}
 
-	influx, err := client.NewHTTPClient(client.HTTPConfig{
-		Addr: conf.influx.url,
-	})
-	if err != nil {
-		logrus.WithError(err).Fatal("creating new influx client")
-	}
-	defer influx.Close()
+	client := influxdb.NewClientWithOptions(
+		conf.Influx.URL,
+		conf.Influx.Token,
+		influxdb.DefaultOptions(),
+	)
 
-	logrus.WithField("baseURL", baseURL.String()).Info("starting loggly poller")
-	loop(influx, lc, conf.interval)
+	rapi := client.WriteAPI(conf.Influx.Org, conf.Influx.Bucket)
+	errCh := rapi.Errors()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for err := range errCh {
+			logrus.WithError(err).Error("writing metrics batch to influx database")
+		}
+	}()
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	wg.Add(1)
+	go func() {
+		defer func() {
+			client.Close()
+			wg.Done()
+		}()
+
+		logrus.WithField("baseURL", baseURL.String()).
+			Info("starting loggly poller")
+
+		pollTags(ctx, rapi, lc, conf.Interval)
+	}()
+
+	// Listen for termination request.
+	terminationCh := make(chan os.Signal, 1)
+	signal.Notify(terminationCh, syscall.SIGINT, syscall.SIGTERM)
+	daemon.SdNotify(false, daemon.SdNotifyReady)
+
+	<-terminationCh
+	daemon.SdNotify(false, daemon.SdNotifyStopping)
+
+	cancel()
+	wg.Wait()
+	logrus.Info("stopped loggly poller")
 }
